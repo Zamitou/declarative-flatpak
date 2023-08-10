cf11d942b7fdcce9362027f5698ecef92b5e8467
Zamitou <141634733+Zamitou@users.noreply.github.com>
2023-08-10 23:32:32 +0000

    Update nixos.nix

--- src/modules/nixos.nix
+++ src/modules/nixos.nix
@@ -18,7 +18,7 @@ in
       ];
       script = "${import ../script.nix {
         inherit config lib pkgs;
-        is-system-install = true;
+        extra-flatpak-flags = [ "--system" ];
       }}";
     };
 
@@ -27,6 +27,10 @@ in
         assertion = cfg.enable;
         message = "This flatpak module is useless if flatpaks are disabled in your config.";
       }
+    ];
+
+    warnings = [
+      "The \"stable\" branch of the flatpak module is about to inherit a lot of the under-the-hood changes from \"dev\".\n    If you want to keep this version, pin it in your flake inputs.\n    The last working commit of this branch is fb31283f55f06b489f2baf920201e8eb73c9a0d3 (commit before this warning was added).\n\nDo not request support.\nDo not override inputs."
     ];
   };
 }
